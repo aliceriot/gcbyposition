575359207485aef9cadcd4853f1f925dda992eba
benpote <ben.pote@gmail.com>
2014-01-09 10:57:53 -0500

    Updated gcbypos.py

--- gcbypos.py
+++ gcbypos.py
@@ -2,9 +2,17 @@ import gzip fin = gzip.open('test_data/heli.R1s.fq.gz', 'rb')
 
 def gcbypos(seqfile):
 	gc = {}
-	for count, txt in enumerate(seqfile):
+	for line in enumerate(seqfile):
+		count, txt = line
 		if count % 4 == 1:
-			for i in len(txt):
-				gc[i].append(txt[i])
+			if count == 1:
+				for i in range(len(txt)):
+					gc[i] = 0
+				for j in range(len(txt)):
+					if txt[j] == 'G' or txt[j] == 'C' or txt[j] == 'g' or txt[j] == 'c':
+						gc[j] += 1
+			else:
+				for j in range(len(txt)):
+					if txt[j] == 'G' or txt[j] == 'C' or txt[j] == 'g' or txt[j] == 'c':
+						gc[j] += 1
 	return gc
-
